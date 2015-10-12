a39e267eeacfdc515604eb223dee7a0e8ac16a21
Manuel <manuel.mayo.lado@gmail.com>
2015-10-12 05:39:06 +0200

    update

--- main.py
+++ main.py
@@ -3,23 +3,25 @@
 ###LIBRERIAS NECESARIAS###
 #pygame
 #pyOpenGL
+#Box2D
 
 from __future__ import division
 
-from modulos.constantes import *
 from modulos.clases import *
 from modulos.funcions import *
+
+from pygame.locals import *
 
 import ctypes
 import os
 import sys
 
 if os.name == 'nt' and sys.getwindowsversion()[0] >= 6:
-    ctypes.windll.user32.SetProcessDPIAware()
+	ctypes.windll.user32.SetProcessDPIAware()
 
 #VARIABLES
 
-camara_libre = False
+camara_libre = True
 mostrar_cadricula = True
 
 _fase_cargada = False
@@ -52,271 +54,267 @@ _ON = True
 
 def main():
 
-    saltando = 10
+	global _ON
+	global _fase_cargada
+	global camara_libre
+	global mostrar_cadricula
+	global ANCHO_PANTALLA_GL
+	global ALTO_PANTALLA_GL
 
-    global _ON
-    global _fase_cargada
-    global camara_libre
-    global mostrar_cadricula
+	#INICIAR OPENGL
 
-    #INICIAR OPENGL
+	init_gl()
 
-    init_gl()
+	#CARGAR TEXTURAS
 
-    #CARGAR TEXTURAS
+	glGenTextures(2,[1,2])
 
-    glGenTextures(2,[1,2])
+	glBindTexture(GL_TEXTURE_2D,1)
+	cargar_imagen_textura("texturas/textura-02.png")
 
-    glBindTexture(GL_TEXTURE_2D,1)
-    cargar_imagen_textura("texturas/textura-02.png")
+	glBindTexture(GL_TEXTURE_2D,2)
+	cargar_imagen_textura("texturas/ceo-01.png")
 
-    glBindTexture(GL_TEXTURE_2D,2)
-    cargar_imagen_textura("texturas/ceo-01.png")
+	#glDeleteTextures([1,2])
 
-    #glDeleteTextures([1,2])
 
+	#BUCLE XOGO
+	#-----------------
 
-    #BUCLE XOGO
-    #-----------------
+	while _ON:
 
-    while _ON:
+		reloj = pygame.time.Clock()
 
-        reloj = pygame.time.Clock()
+		#### CARGA DE FASE ####
+		#######################
 
-        #### CARGA DE FASE ####
-        #######################
+		if not _fase_cargada:
 
-        if not _fase_cargada:
+			fase_actual = lista_fases[num_fase]
 
-            fase_actual = lista_fases[num_fase]
+			#VARIABLES
 
-            #VARIABLES
+			cargar_fase(fase_actual)
 
-            cargar_fase(fase_actual)
+			(NUM_CADROS_ANCHO_FASE,NUM_CADROS_ALTO_FASE,NUM_CADROS_TOTALES_FASE,lista_cadros_colision,
+			 ANCHO_FASE,ALTO_FASE,lista_vertices_cadros_colision,vertices_cadricula) = cargar_fase(fase_actual)
 
-            (NUM_CADROS_ANCHO_FASE,NUM_CADROS_ALTO_FASE,NUM_CADROS_TOTALES_FASE,lista_cadros_colision,
-             ANCHO_FASE,ALTO_FASE,lista_vertices_cadros_colision,vertices_cadricula) = cargar_fase(fase_actual)
+			#LISTAS DE OPENGL
 
-            #LISTAS DE OPENGL
+				#CADROS_COLISION
 
-                #CADROS_COLISION
+			ID_LISTA_CADROS_COLISION = glGenLists(1)
+			crear_lista(ID_LISTA_CADROS_COLISION,lista_vertices_cadros_colision,"rectangulo")
 
-            ID_LISTA_CADROS_COLISION = glGenLists(2)
-            crear_lista(ID_LISTA_CADROS_COLISION,lista_vertices_cadros_colision,"rectangulo")
+				#CADRICULA
 
-                #CADRICULA
+			ID_LISTA_CADRICULA = glGenLists(1)
+			glBindTexture(GL_TEXTURE_2D, 1)
+			crear_lista(ID_LISTA_CADRICULA,vertices_cadricula,"liña")
 
-            ID_LISTA_CADRICULA = glGenLists(1)
-            glBindTexture(GL_TEXTURE_2D, 1)
-            crear_lista(ID_LISTA_CADRICULA,vertices_cadricula,"liña")
+			_fase_cargada = True
 
-            _fase_cargada = True
+		#######################
+		#######################
 
-        #######################
-        #######################
+		#LIMPIAR VENTANA
 
-        #LIMPIAR VENTANA
+		limpiar_ventana_gl(ANCHO_PANTALLA_GL,ALTO_PANTALLA_GL)
 
-        limpiar_ventana_gl()
+		glBindTexture(GL_TEXTURE_2D,0)
 
-        glBindTexture(GL_TEXTURE_2D,0)
+		############################################
+		#DEBUXADO
+		############################################
 
-        ############################################
-        #DEBUXADO
-        ############################################
+		#DEBUXAR FONDO
+		glColor4f(1, 1, 1, 1)
+		glLoadIdentity()
+		glBegin(GL_QUADS)
+		glVertex2f(pos_camara[0],pos_camara[1])
+		glVertex2f(ANCHO_PANTALLA_GL+pos_camara[0],pos_camara[1])
+		glVertex2f(ANCHO_PANTALLA_GL+pos_camara[0],ALTO_PANTALLA_GL+pos_camara[1])
+		glVertex2f(pos_camara[0],ALTO_PANTALLA_GL+pos_camara[1])
+		glEnd()
 
-        #DEBUXAR FONDO
+		glColor4f(0.8,0.8,0.8,0.5)
+		glLoadIdentity()
+		glBindTexture(GL_TEXTURE_2D,2)
+		glBegin(GL_QUADS)
+		glTexCoord2f(0,0)
+		glVertex2f(0,0)
+		glTexCoord2f(1,0)
+		glVertex2f(ANCHO_FASE,0)
+		glTexCoord2f(1,1)
+		glVertex2f(ANCHO_FASE,ALTO_FASE)
+		glTexCoord2f(0,1)
+		glVertex2f(0,ALTO_FASE)
+		glEnd()
 
-        glColor4f(1, 1, 1, 1)
-        glLoadIdentity()
-        glBegin(GL_QUADS)
-        glVertex2f(pos_camara[0],pos_camara[1])
-        glVertex2f(ANCHO_PANTALLA_GL+pos_camara[0],pos_camara[1])
-        glVertex2f(ANCHO_PANTALLA_GL+pos_camara[0],ALTO_PANTALLA_GL+pos_camara[1])
-        glVertex2f(pos_camara[0],ALTO_PANTALLA_GL+pos_camara[1])
-        glEnd()
+		#CADROS_COLISION
+		glColor4f(0.8,0.8,0.8,1)
+		glBindTexture(GL_TEXTURE_2D,1)
+		glCallList(ID_LISTA_CADROS_COLISION)
 
-        glColor4f(0.8,0.8,0.8,0.5)
-        glLoadIdentity()
-        glBindTexture(GL_TEXTURE_2D,2)
-        glBegin(GL_QUADS)
-        glTexCoord2f(0,0)
-        glVertex2f(0,0)
-        glTexCoord2f(1,0)
-        glVertex2f(ANCHO_FASE,0)
-        glTexCoord2f(1,1)
-        glVertex2f(ANCHO_FASE,ALTO_FASE)
-        glTexCoord2f(0,1)
-        glVertex2f(0,ALTO_FASE)
-        glEnd()
+		#QUITAR TEXTURAS
+		glBindTexture(GL_TEXTURE_2D,0)
 
-        #CADROS_COLISION
+		#DEBUXAR PJ
+		glColor4f(0.9, 0.7, 0, 0.8)
+		debuxar_pj(pj.pos)
+
+		#CADRICULA
+		glColor4f(0.5, 0.5, 0.5, 0.5)
+		if mostrar_cadricula:
+			glCallList(ID_LISTA_CADRICULA)
+
+		###########################################
+		#FISICA
+		###########################################
 
-        glColor4f(0.8,0.8,0.8,1)
-        glBindTexture(GL_TEXTURE_2D,1)
-        glCallList(ID_LISTA_CADROS_COLISION)
+		l_cadros_inferiores = cadros_inferiores(pj,lista_cadros_colision,NUM_CADROS_ANCHO_FASE)
+		distancia = distancia_sujeto_cadro(pj,l_cadros_inferiores)
 
-        #QUITAR TEXTURAS
-        glBindTexture(GL_TEXTURE_2D,0)
-
-        #glBindTexture(GL_TEXTURE_2D,False)
-
-        #DEBUXAR PJ
-
-        glColor4f(0, 0.9, 0.3, 1)
-        debuxar_rect_gl(
-            [(pj.pos[0],pj.pos[1]),
-             (pj.pos[0]+ANCHO_CADRO,pj.pos[1]),
-             (pj.pos[0]+ANCHO_CADRO,pj.pos[1]+ALTO_CADRO*2),
-             (pj.pos[0],pj.pos[1]+ALTO_CADRO*2)]
-        )
-
-        #CADRICULA
-
-        glColor4f(0.5, 0.5, 0.5, 0.5)
-
-        if mostrar_cadricula:
-            glCallList(ID_LISTA_CADRICULA)
-
-        ###########################################
-        #FISICA
-        ###########################################
-
-        l_cadros_inferiores = cadros_inferiores(pj,lista_cadros_colision,NUM_CADROS_ANCHO_FASE)
-        distancia = distancia_sujeto_cadro(pj,l_cadros_inferiores)
-
-        glColor4f(1,0,0,0.5)
-        for i in l_cadros_inferiores:
-            debuxar_rect_2(i[0]*ANCHO_CADRO,i[1]*ALTO_CADRO,i[2],i[3])
-
-        if distancia <= 0 and pj.fisica.vel[1] <= 0:
-            pj.fisica.vel[1] = 0
-            if abs(distancia) < ALTO_CADRO and abs(distancia) > 1:
-                pj.pos = [pj.pos[0],pj.pos[1]-distancia]
-            pj_en_suelo = True
-        else:
-            pj.fisica.vel[1] -= pj.fisica.gravedad
-            pj_en_suelo = False
-
-        for impulso in pj.fisica.impulsos:
-            pj.fisica.vel[0] += impulso[0]
-            pj.fisica.vel[1] += impulso[1]
-            impulso[2] -= 1
-
-        for impulso in pj.fisica.impulsos:
-            if impulso[2] <= 0:
-                pj.fisica.impulsos.remove(impulso)
-
-        pj.pos = pj.pos[0]+pj.fisica.vel[0],pj.pos[1]+pj.fisica.vel[1]
-
-        #CAMARA
-
-        if camara_libre:
-            if tecla_pulsada[K_RIGHT]:
-                pos_camara[0] += 1
-            if tecla_pulsada[K_LEFT]:
-                pos_camara[0] -= 1
-            if tecla_pulsada[K_UP]:
-                pos_camara[1] += 1
-            if tecla_pulsada[K_DOWN]:
-                pos_camara[1] -= 1
-        else:
-            pos_camara[0] = pj.pos[0]-(ANCHO_PANTALLA_GL/2-ANCHO_CADRO/2)
-            pos_camara[1] = pj.pos[1]-(ALTO_PANTALLA_GL/2-ALTO_CADRO)
-
-            pos_camara[0] = max(pos_camara[0], 0)
-            pos_camara[0] = min(pos_camara[0], ANCHO_FASE-ANCHO_PANTALLA_GL)
-
-            pos_camara[1] = max(pos_camara[1], 0)
-            pos_camara[1] = min(pos_camara[1], ALTO_FASE-ALTO_PANTALLA_GL)
-
-
-        ############################################
-        #EVENTOS
-        ############################################
-
-        ###### TECLAS PULSADAS ######
-
-        tecla_pulsada = pygame.key.get_pressed()
-
-        if tecla_pulsada[K_d]:
-            pj.fisica.vel[0] = 1
-
-        if tecla_pulsada[K_a]:
-            pj.fisica.vel[0] = -1
-
-        if tecla_pulsada[K_d] and tecla_pulsada[K_a] or not(tecla_pulsada[K_d] or tecla_pulsada[K_a]):
-            pj.fisica.vel[0] = 0
-
-        if tecla_pulsada[K_w] and pj_en_suelo and not saltando:
-            saltando = 10
-            if len(pj.fisica.impulsos) == 0:
-                pj.fisica.impulsos.append([0,0.4,5])
-
-        saltando = max(0,saltando-1)
-
-        ####### MOUSE ########
-
-        pos_mouse = pygame.mouse.get_pos()
-
-        if pos_mouse[0] >= MARCO_LATERAL/2 and pos_mouse[0] <= ANCHO_VENTANA-MARCO_LATERAL/2:
-            pos_mouse_gl = [(pos_mouse[0]-MARCO_LATERAL/2)*ANCHO_PANTALLA_GL/(ANCHO_VENTANA-MARCO_LATERAL)+pos_camara[0],
-                        ALTO_PANTALLA_GL-(pos_mouse[1]*ALTO_PANTALLA_GL/ALTO_VENTANA)+pos_camara[1]]
-        else:
-            pos_mouse_gl = False
-
-
-        if pos_mouse_gl:
-            glColor4f(1, 0, 0, 0.4)
-            debuxar_linha([[pos_mouse_gl[0],pos_camara[1]],[pos_mouse_gl[0],ALTO_PANTALLA_GL+pos_camara[1]]])
-            debuxar_linha([[pos_camara[0],pos_mouse_gl[1]],[ANCHO_PANTALLA_GL+pos_camara[0],pos_mouse_gl[1]]])
-            if pos_mouse_gl[0] < 0:
-                pos_mouse_gl[0]-= ANCHO_CADRO
-            if pos_mouse_gl[1] < 0:
-                pos_mouse_gl[1] -= ALTO_CADRO
-            debuxar_rect_gl(
-                [[int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO],
-                [int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO+ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO],
-                [int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO+ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO+ALTO_CADRO],
-                [int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO+ALTO_CADRO]])
-
-
-        #EVENTOS
-
-        for evento in pygame.event.get():
-
-            #TECLADO
-            if evento.type == pygame.KEYDOWN:
-
-                #CAMARA_LIBRE
-                if evento.key == K_c:
-                    if camara_libre:
-                        camara_libre=False
-                    else:
-                        camara_libre=True
-
-                #MOSTRAR_CADRICULA
-                if evento.key == K_v:
-                    if mostrar_cadricula:
-                        mostrar_cadricula = False
-                    else:
-                        mostrar_cadricula = True
-
-                #ESC - CERRAR  XOGO
-                if evento.key == K_ESCAPE:
-                    _ON = False
-
-            #QUIT
-            if evento.type == pygame.QUIT:
-                _ON = False
-
-        if not _ON:
-            pygame.display.quit()
-            break
-
-        pygame.display.flip()
-
-        reloj.tick(FPS)
+		if distancia <= 0 and pj.fisica.vel[1] <= 0:
+			pj.fisica.vel[1] = 0
+			if ALTO_CADRO > abs(distancia) > 1:
+				pj.pos = [pj.pos[0],pj.pos[1]-distancia]
+			pj_en_suelo = True
+		else:
+			pj.fisica.vel[1] -= pj.fisica.gravedad
+			pj_en_suelo = False
+
+		for impulso in pj.fisica.impulsos:
+			pj.fisica.vel[0] += impulso[0]
+			pj.fisica.vel[1] += impulso[1]
+			impulso[2] -= 1
+
+		for impulso in pj.fisica.impulsos:
+			if impulso[2] <= 0:
+				pj.fisica.impulsos.remove(impulso)
+
+		pj.pos = pj.pos[0]+pj.fisica.vel[0],pj.pos[1]+pj.fisica.vel[1]
+
+
+		############################################
+		#EVENTOS
+		############################################
+
+		###### TECLAS PULSADAS ######
+
+		tecla_pulsada = pygame.key.get_pressed()
+
+		if tecla_pulsada[K_d]:
+			pj.fisica.vel[0] = 1
+
+		if tecla_pulsada[K_a]:
+			pj.fisica.vel[0] = -1
+
+		if tecla_pulsada[K_d] and tecla_pulsada[K_a] or not(tecla_pulsada[K_d] or tecla_pulsada[K_a]):
+			pj.fisica.vel[0] = 0
+
+		if tecla_pulsada[K_w] and pj_en_suelo:
+			if len(pj.fisica.impulsos) == 0:
+				pj.fisica.impulsos.append([0,0.4,5])
+
+				#CAMARA
+
+		if camara_libre:
+			if tecla_pulsada[K_RIGHT]:
+				pos_camara[0] += 1
+			if tecla_pulsada[K_LEFT]:
+				pos_camara[0] -= 1
+			if tecla_pulsada[K_UP]:
+				pos_camara[1] += 1
+			if tecla_pulsada[K_DOWN]:
+				pos_camara[1] -= 1
+		else:
+			pos_camara[0] = pj.pos[0]-(ANCHO_PANTALLA_GL/2-ANCHO_CADRO/2)
+			pos_camara[1] = pj.pos[1]-(ALTO_PANTALLA_GL/2-ALTO_CADRO)
+
+			pos_camara[0] = max(pos_camara[0], 0)
+			pos_camara[0] = min(pos_camara[0], ANCHO_FASE-ANCHO_PANTALLA_GL)
+
+			pos_camara[1] = max(pos_camara[1], 0)
+			pos_camara[1] = min(pos_camara[1], ALTO_FASE-ALTO_PANTALLA_GL)
+
+
+		####### MOUSE ########
+
+		pos_mouse = pygame.mouse.get_pos()
+
+		if (MARCO_LATERAL/2 <= pos_mouse[0] <= ANCHO_VENTANA-MARCO_LATERAL/2
+			and MARCO_VERTICAL / 2 <= pos_mouse[1] <= ALTO_VENTANA - MARCO_VERTICAL / 2):
+			pos_mouse_gl = [
+				(pos_mouse[0]-MARCO_LATERAL/2)*ANCHO_PANTALLA_GL/(ANCHO_VENTANA-MARCO_LATERAL)+pos_camara[0],
+				ALTO_PANTALLA_GL-(
+					(pos_mouse[1]-MARCO_VERTICAL/2)*ALTO_PANTALLA_GL/(ALTO_VENTANA-MARCO_VERTICAL))+pos_camara[1]]
+		else:
+			pos_mouse_gl = False
+
+		if pos_mouse_gl:
+			glColor4f(1, 0, 0, 0.3)
+			if pos_mouse_gl[0] < 0:
+				pos_mouse_gl[0]-= ANCHO_CADRO
+			if pos_mouse_gl[1] < 0:
+				pos_mouse_gl[1] -= ALTO_CADRO
+			if 0 <= pos_mouse_gl[0] <= ANCHO_FASE and 0 <= pos_mouse_gl[1] <= ALTO_FASE:
+				debuxar_rect_gl(
+					[[int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO],
+					[int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO+ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO],
+					[int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO+ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO+ALTO_CADRO],
+					[int(pos_mouse_gl[0]/ANCHO_CADRO)*ANCHO_CADRO,int(pos_mouse_gl[1]/ALTO_CADRO)*ALTO_CADRO+ALTO_CADRO]])
+
+
+		#EVENTOS
+
+		for evento in pygame.event.get():
+
+			#MOUSE
+			if evento.type == pygame.MOUSEBUTTONDOWN:
+				if evento.button == 4:
+					ANCHO_PANTALLA_GL -= 3
+					if camara_libre:
+						pos_camara[0] += 1.5
+				elif evento.button == 5:
+					ANCHO_PANTALLA_GL += 3
+					if camara_libre:
+						pos_camara[0] -= 1.5
+				if evento.button in [4,5]:
+					ALTO_PANTALLA_GL = ANCHO_PANTALLA_GL / DIF_ASP
+
+			#TECLADO
+			if evento.type == pygame.KEYDOWN:
+
+				#CAMARA_LIBRE
+				if evento.key == K_c:
+					if camara_libre:
+						camara_libre=False
+					else:
+						camara_libre=True
+
+				#MOSTRAR_CADRICULA
+				if evento.key == K_v:
+					if mostrar_cadricula:
+						mostrar_cadricula = False
+					else:
+						mostrar_cadricula = True
+
+				#ESC - CERRAR  XOGO
+				if evento.key == K_ESCAPE:
+					_ON = False
+
+			#QUIT
+			if evento.type == pygame.QUIT:
+				_ON = False
+
+		if not _ON:
+			pygame.display.quit()
+			break
+
+		pygame.display.flip()
+
+		reloj.tick(FPS)
 
 if __name__ == '__main__':
-    main()
+	main()
--- modulos/funcions.py
+++ modulos/funcions.py
@@ -15,144 +15,142 @@ def num(p, num_cadros_ancho_fase):
 	return p[0]+p[1]*num_cadros_ancho_fase
 
 def cargar_lista_cadros_colision(archivo,lista):
-    text = open(archivo,"r").read()
-    cont = 0
-    for i in text:
-        lista[cont] = int(i)
-        cont += 1
-    return lista
+	text = open(archivo,"r").read()
+	cont = 0
+	for i in text:
+		lista[cont] = int(i)
+		cont += 1
+	return lista
 
 def cargar_fase(fase):
-    lista_cadros_colision = []
-    for i in range(fase.num_cadros):
-        lista_cadros_colision.append(0)
-    lista_cadros_colision = cargar_lista_cadros_colision(fase.doc_col,lista_cadros_colision)
+	lista_cadros_colision = []
+	for i in range(fase.num_cadros):
+		lista_cadros_colision.append(0)
+	lista_cadros_colision = cargar_lista_cadros_colision(fase.doc_col,lista_cadros_colision)
 
-    lista_vertices_cadros_colision = []
-    for i in range(len(lista_cadros_colision)):
-        if lista_cadros_colision[i]:
-            pos_cadro = pos(i,fase.cadros_ancho)
-            lista_vertices_cadros_colision.append(
-                [pos_cadro[0]*ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO])
-            lista_vertices_cadros_colision.append(
-                [pos_cadro[0]*ANCHO_CADRO+ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO])
-            lista_vertices_cadros_colision.append(
-                [pos_cadro[0]*ANCHO_CADRO+ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO+ALTO_CADRO])
-            lista_vertices_cadros_colision.append(
-                [pos_cadro[0]*ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO+ALTO_CADRO])
+	lista_vertices_cadros_colision = []
+	for i in range(len(lista_cadros_colision)):
+		if lista_cadros_colision[i]:
+			pos_cadro = pos(i,fase.cadros_ancho)
+			lista_vertices_cadros_colision.append(
+				[pos_cadro[0]*ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO])
+			lista_vertices_cadros_colision.append(
+				[pos_cadro[0]*ANCHO_CADRO+ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO])
+			lista_vertices_cadros_colision.append(
+				[pos_cadro[0]*ANCHO_CADRO+ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO+ALTO_CADRO])
+			lista_vertices_cadros_colision.append(
+				[pos_cadro[0]*ANCHO_CADRO,pos_cadro[1]*ALTO_CADRO+ALTO_CADRO])
 
-    vertices_cadricula = []
+	vertices_cadricula = []
 
-    for i in range(fase.cadros_ancho+1):
-        vertices_cadricula.append([i*ANCHO_CADRO,0])
-        vertices_cadricula.append([i*ANCHO_CADRO,fase.alto])
+	for i in range(fase.cadros_ancho+1):
+		vertices_cadricula.append([i*ANCHO_CADRO,0])
+		vertices_cadricula.append([i*ANCHO_CADRO,fase.alto])
 
-    for i in range(fase.cadros_alto+1):
-        vertices_cadricula.append([0,i*ALTO_CADRO])
-        vertices_cadricula.append([fase.ancho,i*ALTO_CADRO])
+	for i in range(fase.cadros_alto+1):
+		vertices_cadricula.append([0,i*ALTO_CADRO])
+		vertices_cadricula.append([fase.ancho,i*ALTO_CADRO])
 
-    return fase.cadros_ancho,fase.cadros_alto,fase.num_cadros,lista_cadros_colision,fase.ancho,fase.alto,lista_vertices_cadros_colision,vertices_cadricula
+	return fase.cadros_ancho,fase.cadros_alto,fase.num_cadros,lista_cadros_colision,fase.ancho,fase.alto,lista_vertices_cadros_colision,vertices_cadricula
 
 #### COLISIÓNS ####
 
 def cadros_inferiores(sujeto,lista,cadrados_ancho):
-    pos_cadro_sujeto = int(sujeto.pos[0]/ANCHO_CADRO), int(sujeto.pos[1]/ALTO_CADRO)
-    num_cadro_sujeto = num(pos_cadro_sujeto,cadrados_ancho)
-    num_a_mirar = [num_cadro_sujeto,num_cadro_sujeto-cadrados_ancho]
-    if not pos_cadro_sujeto[0] <= 0 and sujeto.pos < pos_cadro_sujeto[0]*ANCHO_CADRO:
-        num_a_mirar.append(num_cadro_sujeto-1)
-        num_a_mirar.append(num_cadro_sujeto-cadrados_ancho-1)
-    if not pos_cadro_sujeto[0] >= cadrados_ancho-1 and sujeto.pos > pos_cadro_sujeto[0]*ANCHO_CADRO:
-        num_a_mirar.append(num_cadro_sujeto+1)
-        num_a_mirar.append(num_cadro_sujeto-cadrados_ancho+1)
-    lista_cadros = []
-    for n in num_a_mirar:
-        try:
-            if lista[n]:
-                lista_cadros.append([pos(n,cadrados_ancho)[0],pos(n,cadrados_ancho)[1],ANCHO_CADRO,ALTO_CADRO])
-        except:
-            None
+	pos_cadro_sujeto = int(sujeto.pos[0]/ANCHO_CADRO), int(sujeto.pos[1]/ALTO_CADRO)
+	num_cadro_sujeto = num(pos_cadro_sujeto,cadrados_ancho)
+	num_a_mirar = [num_cadro_sujeto,num_cadro_sujeto-cadrados_ancho]
+	if not pos_cadro_sujeto[0] <= 0 and sujeto.pos < pos_cadro_sujeto[0]*ANCHO_CADRO:
+		num_a_mirar.append(num_cadro_sujeto-1)
+		num_a_mirar.append(num_cadro_sujeto-cadrados_ancho-1)
+	if not pos_cadro_sujeto[0] >= cadrados_ancho-1 and sujeto.pos > pos_cadro_sujeto[0]*ANCHO_CADRO:
+		num_a_mirar.append(num_cadro_sujeto+1)
+		num_a_mirar.append(num_cadro_sujeto-cadrados_ancho+1)
+	lista_cadros = []
+	for n in num_a_mirar:
+		try:
+			if lista[n]:
+				lista_cadros.append([pos(n,cadrados_ancho)[0],pos(n,cadrados_ancho)[1],ANCHO_CADRO,ALTO_CADRO])
+		except:
+			None
 
-    return lista_cadros
+	return lista_cadros
 
 def distancia_sujeto_cadro(sujeto,lista_cadros):
-    conxunto_y = []
-    if lista_cadros:
-        for i in lista_cadros:
-            conxunto_y.append(i[1])
-        y = max(conxunto_y)
-        distancia = sujeto.pos[1]-(y*ALTO_CADRO+ALTO_CADRO)
-        return distancia
-    else:
-        return "nada"
+	conxunto_y = []
+	if lista_cadros:
+		for i in lista_cadros:
+			conxunto_y.append(i[1])
+		y = max(conxunto_y)
+		distancia = sujeto.pos[1]-(y*ALTO_CADRO+ALTO_CADRO)
+		return distancia
+	else:
+		return "nada"
 
 
 #### OPENGL ####
 
 def init_gl():
-    glViewport(MARCO_LATERAL/2,0,ANCHO_VENTANA-MARCO_LATERAL,ALTO_VENTANA)
-    glClearColor(0,0,0,0)
-    glEnable(GL_TEXTURE_2D)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    #glEnable(GL_LINE_SMOOTH)
-    #glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
+	glViewport(MARCO_LATERAL/2,MARCO_VERTICAL/2,ANCHO_VENTANA-MARCO_LATERAL,ALTO_VENTANA-MARCO_VERTICAL)
+	glClearColor(0,0,0,0)
+	glEnable(GL_TEXTURE_2D)
+	glEnable(GL_BLEND)
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+	#glEnable(GL_LINE_SMOOTH)
+	#glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
 
 
-def limpiar_ventana_gl():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluOrtho2D(0,DIMENSIONS_GL_ESTANDAR,0,DIMENSIONS_GL)
-    glTranslatef(0-pos_camara[0],0-pos_camara[1],0)
-    glMatrixMode(GL_MODELVIEW)
+def limpiar_ventana_gl(ancho_gl,alto_gl):
+	glClear(GL_COLOR_BUFFER_BIT)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	gluOrtho2D(0,ancho_gl,0,alto_gl)
+	glTranslatef(0-pos_camara[0],0-pos_camara[1],0)
+	glMatrixMode(GL_MODELVIEW)
 
-def debuxar_rect_gl(vertices,id_text=False):
-    glLoadIdentity()
-    glBegin(GL_QUADS)
-    for v in range(0,len(vertices),4):
-        glTexCoord2f(0,0)
-        glVertex2f(vertices[v][0],vertices[v][1])
-        glTexCoord2f(1,0)
-        glVertex2f(vertices[v+1][0],vertices[v+1][1])
-        glTexCoord2f(1,1)
-        glVertex2f(vertices[v+2][0],vertices[v+2][1])
-        glTexCoord2f(0,1)
-        glVertex2f(vertices[v+3][0],vertices[v+3][1])
-    glEnd()
+def debuxar_rect_gl(vertices,pos=False):
+	glLoadIdentity()
+	if pos:
+		glTranslatef(pos[0], pos[1], 0)
+	glBegin(GL_QUADS)
+	for v in range(0,len(vertices),4):
+		glTexCoord2f(0,0)
+		glVertex2f(vertices[v][0],vertices[v][1])
+		glTexCoord2f(1,0)
+		glVertex2f(vertices[v+1][0],vertices[v+1][1])
+		glTexCoord2f(1,1)
+		glVertex2f(vertices[v+2][0],vertices[v+2][1])
+		glTexCoord2f(0,1)
+		glVertex2f(vertices[v+3][0],vertices[v+3][1])
+	glEnd()
 
-def debuxar_rect_2(x,y,ancho,alto):
-    glLoadIdentity()
-    glBegin(GL_QUADS)
-    glVertex2f(x,y)
-    glVertex2f(x+ancho,y)
-    glVertex2f(x+ancho,y+alto)
-    glVertex2f(x,y+alto)
-    glEnd()
+def debuxar_pj(posicion):
+	debuxar_rect_gl(
+	[[-ANCHO_CADRO/2,-ALTO_CADRO],
+	 [ANCHO_CADRO/2,-ALTO_CADRO],
+	 [ANCHO_CADRO/2,ALTO_CADRO],
+	 [-ANCHO_CADRO/2,ALTO_CADRO]],
+	[posicion[0]+ANCHO_CADRO/2,posicion[1]+ALTO_CADRO])
 
 def debuxar_linha(vertices):
-    glLoadIdentity()
-    glBegin(GL_LINES)
-    for v in vertices:
-        glVertex2f(v[0],v[1])
-    glEnd()
+	glLoadIdentity()
+	glBegin(GL_LINES)
+	for v in vertices:
+		glVertex2f(v[0],v[1])
+	glEnd()
 
 def cargar_imagen_textura(imagen):
-    texturaSurface = pygame.image.load(imagen).convert_alpha()
-    texturaData = pygame.image.tostring(texturaSurface, "RGBA", True)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texturaSurface.get_width(), texturaSurface.get_height(), 0,
-                           GL_RGBA, GL_UNSIGNED_BYTE, texturaData)
+	texturaSurface = pygame.image.load(imagen).convert_alpha()
+	texturaData = pygame.image.tostring(texturaSurface, "RGBA", True)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texturaSurface.get_width(), texturaSurface.get_height(), 0,
+						   GL_RGBA, GL_UNSIGNED_BYTE, texturaData)
 
-def crear_lista(id_lista,vertices,forma,id_textura=False):
-    glNewList(id_lista, GL_COMPILE) ### INICIO LISTA
-    glLoadIdentity()
-    if forma=="liña":
-        debuxar_linha(vertices)
-    elif forma=="rectangulo":
-        if id_textura:
-            debuxar_rect_gl(vertices,id_textura)
-        else:
-            debuxar_rect_gl(vertices)
-    glEndList() ############################### FIN LISTA
+def crear_lista(id_lista,vertices,forma):
+	glNewList(id_lista, GL_COMPILE) ### INICIO LISTA
+	glLoadIdentity()
+	if forma=="liña":
+		debuxar_linha(vertices)
+	elif forma=="rectangulo":
+		debuxar_rect_gl(vertices)
+	glEndList() ############################### FIN LISTA
